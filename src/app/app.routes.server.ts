7470a9b95b216c6685000b28dd7207b4fb92dd59
m03433amed-blip <m03433amed@gmail.com>
2026-04-26 15:35:07 +0300

    fix: prerender issue

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,26 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'details/:id/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'brands/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'categories/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'subcategories/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
